33eb8199af762bc6466dbe9d1f4ca790eeeafe8c
Vinícius Fagundes <mvlacerda@gmail.com>
2015-10-30 18:32:01 -0200

    Fix bug

--- src/ZfTable/Decorator/Service/DecoratorPluginManagerFactory.php
+++ src/ZfTable/Decorator/Service/DecoratorPluginManagerFactory.php
@@ -19,13 +19,13 @@ class DecoratorPluginManagerFactory implements FactoryInterface
 
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        $configuration   = $serviceLocator->has('Config') ? $serviceLocator->get('Config') : array();
-        $configSeviceMgr = new ConfigServiceMgr(isset($configuration['zftable_decorators'])? : array());
+        $config       = $serviceLocator->has('Config') ? $serviceLocator->get('Config') : array();
+        $configSevice = new ConfigServiceMgr(isset($config['zftable_decorators']) ? $config['zftable_decorators'] : array());
 
-        $plugins = new DecoratorPluginManager($configSeviceMgr);
+        $plugins = new DecoratorPluginManager($configSevice);
         $plugins->setServiceLocator($serviceLocator);
 
-        if (isset($configuration['di']) && $serviceLocator->has('Di')) {
+        if (isset($config['di']) && $serviceLocator->has('Di')) {
             $plugins->addAbstractFactory($serviceLocator->get('DiAbstractServiceFactory'));
         }
 
